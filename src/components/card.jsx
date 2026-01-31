bfbcd4fe590cf22eac216d7eef38e8ded6f1ff29
Palak-Keswani <keswanipalak5@gmail.com>
2026-01-31 20:29:13 +0530

    Done necessary changes and used icons via lucide-icon, made TodaysMode responsive

---
+++ src/components/card.jsx
@@ -0,0 +1,7 @@
+export default function Card(){
+    return(
+        <div className="bg-slate-700">
+            Complete React 
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import PomodoroSettings from './components/PomodoroSettings';
-import Pomodoro from './components/Pomodoro';
-import TodaysPage from './pages/todays-page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodaysPage/>
+    <App/>
   </React.StrictMode>
 );
 
